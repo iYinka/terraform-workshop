4c1610131dd0bc685b51682ba266de613fe969b3
iYinka <olayinkav28@gmail.com>
2024-07-10 23:29:14 +0100

    Added key pair from AWS

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,13 +1,8 @@
-resource "aws_key_pair" "terraform-demo" {
-  key_name   = "terraform-demo"
-  public_key = file("terraform-demo.pub")
-}
-
 resource "aws_instance" "my-instance" {
   count         = var.INSTANCE_COUNT
   ami           = var.AMI[var.REGION]
   instance_type = var.INSTANCE_TYPE
-  key_name      = aws_key_pair.terraform-demo.key_name
+  key_name      = "DevOps-US-East-1"
   subnet_id     = "subnet-0e4e5d490c78c6d1c"
 
   tags = {
